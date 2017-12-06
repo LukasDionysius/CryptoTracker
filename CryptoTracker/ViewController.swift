b3919b2e17e2f648d07a73858ab10d113352160b
Lukas Michaels <lukasdmichaels@gmail.com>
2017-12-06 17:19:37 -0500

    Add current price feature

--- CryptoTracker/ViewController.swift
+++ CryptoTracker/ViewController.swift
@@ -7,13 +7,42 @@
 //
 
 import UIKit
-
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        ///
+        priceLabel.text = "..."
+
+        Alamofire.request("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD").responseJSON { response in
+            print(response)
+    
+            if let bitcoinJSON = response.result.value {
+                let bitcoinObject:Dictionary = bitcoinJSON as! Dictionary<String, Any>
+                
+                // Parse through information
+                let rawObject:Dictionary = bitcoinObject["RAW"] as! Dictionary<String, Any>
+                let usdObject:Dictionary = rawObject["BTC"] as! Dictionary<String, Any>
+                let rateObject:Dictionary = usdObject["USD"] as! Dictionary<String, Any>
+                
+                // Current price
+                let rate:Float = rateObject["PRICE"] as! Float
+                
+                
+                // Changing UI
+                self.priceLabel.text = "S\(rate)"
+            }
+            
+            
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
