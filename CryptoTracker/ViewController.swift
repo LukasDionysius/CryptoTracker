c5d3edfc23d5e1d12bdaea7d85fb2a1f1fc2feb3
Lukas Michaels <lukasdmichaels@gmail.com>
2017-12-06 16:49:13 -0500

    Initial commit

---
+++ CryptoTracker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CryptoTracker
+//
+//  Created by Lukas Michaels on 12/6/17.
+//  Copyright © 2017 Lukas Michaels. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
