86d4703c4026b9a6b532d5f4f136a0cb18157db9
Lukas Michaels <lukasdmichaels@gmail.com>
2017-12-07 18:42:48 -0500

    Add new UI

--- CryptoTracker/ViewController.swift
+++ CryptoTracker/ViewController.swift
@@ -14,6 +14,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var highLabel: UILabel!
     @IBOutlet weak var lowLabel: UILabel!
+    @IBOutlet weak var cryptoLabel: UILabel!
+    
+    // Refresh button
+    @IBAction func refreshBtn(_ sender: Any) {
+        
+        print("Refresh pressed")
+        
+        // Currency formatter
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        
+        ///
+        priceLabel.text = "..."
+        highLabel.text = "..."
+        lowLabel.text = "..."
+        
+        Alamofire.request("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD").responseJSON { response in
+            
+            if let bitcoinJSON = response.result.value {
+                let bitcoinObject:Dictionary = bitcoinJSON as! Dictionary<String, Any>
+                
+                // Parse through information
+                let rawObject:Dictionary = bitcoinObject["RAW"] as! Dictionary<String, Any>
+                let usdObject:Dictionary = rawObject["BTC"] as! Dictionary<String, Any>
+                let btcObject:Dictionary = usdObject["USD"] as! Dictionary<String, Any>
+                
+                // Current price
+                let rate:NSNumber = btcObject["PRICE"] as! NSNumber
+                let rateCurrency = (formatter.string(from: rate)!)
+                // High day price
+                let highDay:NSNumber = btcObject["HIGH24HOUR"] as! NSNumber
+                let highDayCurrency = (formatter.string(from: highDay)!)
+                // Low day price
+                let lowDay:NSNumber = btcObject["LOW24HOUR"] as! NSNumber
+                let lowDayCurrency =  (formatter.string(from: lowDay)!)
+                
+                
+                // Changing UI
+                self.priceLabel.text = "\(rateCurrency)" // Current price
+                self.highLabel.text = "\(highDayCurrency)"
+                self.lowLabel.text = "\(lowDayCurrency)"
+                
+                print("Refresh succeed")
+            }
+    }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +73,7 @@ class ViewController: UIViewController {
         highLabel.text = "..."
         lowLabel.text = "..."
 
+        
         
         // Calling API using Alamofire and putting it in dictionaries
         Alamofire.request("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD").responseJSON { response in
